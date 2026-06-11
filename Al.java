1360c1cc7190c22c13370d71ab26c745efe941df
Sandeep <kurubasandeep93@gmail.com>
2026-06-11 12:03:39 +0530

    Set

---
+++ Al.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       ArrayList<String>arr=new ArrayList<>();
+       arr.add("sandeep");
+       arr.add("venky");
+       arr.add("vara");
+       arr.add("yesh");
+       arr.add("siva");
+       arr.add("likith");
+       arr.add("san");
+       
+       
+       
+       System.out.println(arr);
+    }
+}
---
+++ Sets.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sets{
+    public static void main(String[]args){
+        System.out.println("set program -Hashset ");
+        Set<Integer>Rollno=new HashSet<>();
+        Rollno.add(1);
+        Rollno.add(2);
+         Rollno.add(3);
+         System.out.println(Rollno);
+    }
+}
